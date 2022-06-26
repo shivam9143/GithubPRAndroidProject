cee4175c42adec5ec0f879a2ac11c109b8695665
shivamkanodia <shivam@rollingbanners.in>
2022-06-27 01:10:10 +0530

    final changes done

--- Prs/src/main/java/com/shivam/prs/ui/PrActivity.kt
+++ Prs/src/main/java/com/shivam/prs/ui/PrActivity.kt
@@ -13,9 +13,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -39,8 +37,8 @@ import com.shivam.prs.ui.ui.theme.GithubPrsTheme
 import com.shivam.prs.viewmodel.PrViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.Flow
-import java.text.SimpleDateFormat
 import java.util.*
+
 
 @AndroidEntryPoint
 class PrActivity : ComponentActivity() {
@@ -111,26 +109,36 @@ fun PrRequestItem(prData: PullRequest, onClick: () -> Unit) {
             modifier = Modifier
                 .clip(RoundedCornerShape(4.dp))
                 .background(MaterialTheme.colors.surface)
+                .padding( bottom = 5.dp, top = 5.dp,
+                    start = 5.dp, end = 5.dp),
+            verticalAlignment = Alignment.CenterVertically
         ) {
             Surface(
-                modifier = Modifier.size(130.dp),
+                modifier = Modifier
+                    .fillMaxHeight(1f)
+                    .width(100.dp),
                 shape = RoundedCornerShape(12.dp),
                 color = MaterialTheme.colors.surface.copy(
                     alpha = 0.2f
                 )
             ) {
-                val image = rememberCoilPainter(
-                    request = prData.user.avatar_url,
-                    fadeIn = true
-                )
-                Image(
-                    painter = image,
-                    contentDescription = null,
+                Box(
                     modifier = Modifier
-                        .height(100.dp)
-                        .clip(shape = RoundedCornerShape(12.dp)),
-                    contentScale = ContentScale.Crop
-                )
+                        .fillMaxSize(1.0f) // it will fill parent box
+                ) {
+                    val image = rememberCoilPainter(
+                        request = prData.user.avatar_url,
+                        fadeIn = true
+                    )
+                    Image(
+                        painter = image,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .height(100.dp)
+                            .clip(shape = RoundedCornerShape(12.dp)),
+                        contentScale = ContentScale.Crop
+                    )
+                }
             }
             Column(
                 modifier = Modifier
@@ -155,6 +163,7 @@ fun PrRequestItem(prData: PullRequest, onClick: () -> Unit) {
                     Text(
                         text = prData.title.toString(),
                         fontWeight = FontWeight.Light,
+                        maxLines = 2,
                         style = TextStyle(fontSize = 22.sp),
                         color = Color.Black,
                         modifier = Modifier.padding(top = 5.dp, start = 5.dp)
